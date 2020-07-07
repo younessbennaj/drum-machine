3b9b4ace2ea1518190de8b6ed308a72416879743
Youness BENNAJ <youness.bennaj@gmail.com>
2020-07-07 10:55:44 +0200

    add a bindAudioElement function that bind audio html element associated to a .drum-pad element

--- index.js
+++ index.js
@@ -3,17 +3,6 @@
 //When I press the trigger key associated with each .drum-pad...
 
 //When a .drum-pad is triggered, a string describing the associated audio clip is displayed as the inner text of the #display element
-let sounds = {
-    q: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-    w: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-    e: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-    a: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
-    s: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
-    d: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
-    z: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
-    x: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
-    c: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-}
 
 //Function constructor of drum element
 function DrumElement(source, triggeredKey) {
@@ -21,15 +10,77 @@ function DrumElement(source, triggeredKey) {
     this.triggeredKey = triggeredKey;
 }
 
-let heater1 = new DrumElement(
-    "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-    "Q"
-)
+DrumElement.prototype.getClicked = function (e) {
+    console.log(this);
+}
 
-console.log(heater1);
+function getElements() {
 
-//array of drum elements
-let elements = [];
+    return [
+        new DrumElement(
+            "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+            "A"
+        ),
+        new DrumElement(
+            "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+            "Z"
+        ),
+        new DrumElement(
+            "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+            "E"
+        ),
+        new DrumElement(
+            "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+            "Q"
+        ),
+        new DrumElement(
+            "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+            "S"
+        ),
+        new DrumElement(
+            "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+            "D"
+        ),
+        new DrumElement(
+            "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+            "W"
+        ),
+        new DrumElement(
+            "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+            "X"
+        ),
+        new DrumElement(
+            "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+            "C"
+        )
+    ]
+}
+//A function that bind audio html element associated to a .drum-pad element
+function bindAudioElement() {
+    //get an array of DrumElement() object
+    var elements = getElements();
+
+    //get our drum pad elements
+    var drums = $(".drum-pad");
+
+    //for i from 0 to drums length
+    for (var i = 0; i < drums.length; i++) {
+        //Add an inner text that corresponds to one keys on the keyboard
+        $(drums[i]).html(elements[i].triggeredKey);
+        //Append an audio html element to a .drum-pad element with an id 
+        //corresponding to the inner text of its parent.
+        $("<audio></audio>")
+            .attr('id', elements[i].triggeredKey)
+            .appendTo(drums[i]);
+        //Add a click event listener to all the .drum-pad elements
+        //here we use bind() to bind this to the DrumElement instance in getClicked()  
+        //by default with jQuery "this" in a handler is binded to the element triggered
+        $(drums[i]).on("click", elements[i].getClicked.bind(elements[i]));
+    }
+}
+
 $(window).on("load", function () {
-    console.log('page loaded');
+
+    bindAudioElement();
+
 });
